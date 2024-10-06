d7bab1de1772b1f582eaac293c1836cb948832ef
Absher K <absheroamac@gmail.com>
2024-10-06 10:47:16 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from "react";
 
 function App() {
+
+  const [time,setTime] = useState(0);
+  const [flag,setFlag] = useState(false)
+  const intervalId = useRef(null);
+
+  const formatTime = (time)=>{
+    const minutes = Math.floor(time/60);
+    const seconds = time%60
+
+    if(seconds<10){
+      return (minutes+":"+"0"+seconds);
+    }
+
+    return (minutes+":"+seconds);
+  }
+
+  const handleStart = ()=>{
+
+    if (!flag){
+
+      setFlag(!flag)
+      intervalId.current = setInterval(()=>
+        setTime(time=>time+1)
+    
+        ,1000)
+
+      }
+
+      else{
+
+        
+          clearInterval(intervalId.current)
+          setFlag(!flag)
+
+      }
+
+    }
+
+
+    const handleReset = ()=>{
+      clearInterval(intervalId.current);
+      setFlag(false);
+      setTime(0)
+    }
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <p>Time :{formatTime(time)}</p>
+      <button onClick={()=>handleStart()}>
+        {flag?"Stop":"Start"}
+      </button>
+      <button onClick={handleReset}>
+      Reset
+      </button>
     </div>
   );
 }
